97ecb6491020b41d13401848cda3e514f65ae654
tushar-1910 <tusharagg19@gmail.com>
2025-03-04 21:43:50 +0530

    updated package.json for delpoyment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 2000;
+const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
